527ee8ac64affc9915b72e5057d852db5f6406b4
Shajahan <shajahanlonely@gmail.com>
2024-01-01 15:58:19 +0530

    project 1

---
+++ js/index-1.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll(".item");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClass();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClass() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
